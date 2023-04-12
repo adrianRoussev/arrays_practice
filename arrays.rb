60fc43970e9e6797d67d4cd4ed0f02ee0359eb93
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-12 18:43:06 -0400

    add array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ pet_names = ["popcorn","camden","ruby","peanut"]
 levels = [1,5,7,13,21]
 grades_out_of_1 =[0.67,0.78,0.85,0.99]
 correct_answer_q1 =[false,false,true,true]
+ pet_names.pop(2)
+#this will remove the last 2 stings from the array called pet_names
+levels.push(23,29) 
+#this will add the integers 23 and 29 to the end of the array called levels
+grades_out_of_1.shift(1)
+#this will remove and output the first value in the array called grades_out_of_1
+correct_answer_q1.unshift(true)
+#this will add the boolean in brackets to the beginning of the array
